f6473eff594e0ca5db0871d3042c691409c7d409
Vladislad T <v.trundenko@everad.com>
2019-08-12 14:44:27 +0300

    vlad

--- app/js/main.js
+++ app/js/main.js
@@ -3,42 +3,52 @@
 //=include ../components/footer/footer.js
 
 
-$('.increase-bottom').slick({
-	slidesToShow: 3,
-	slidesToScroll: 3,
-	arrows: false,
-	responsive: [
-	{
-		breakpoint: 1024,
-		settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2,
-			arrows: true
-		}
-	},
-	{
-		breakpoint: 767,
-		settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2,
-			arrows: true
+
+
+// $('.increase-bottom').slick({
+// 	slidesToShow: 3,
+// 	slidesToScroll: 3,
+// 	arrows: false,
+// 	responsive: [
+// 	{
+// 		breakpoint: 1024,
+// 		settings: {
+// 			slidesToShow: 2,
+// 			slidesToScroll: 2,
+// 			arrows: true
+// 		}
+// 	},
+// 	{
+// 		breakpoint: 767,
+// 		settings: {
+// 			slidesToShow: 2,
+// 			slidesToScroll: 2,
+// 			arrows: true
 			
-		}
-	},
-	{
-		breakpoint: 640,
-		settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: true,
-			adaptiveHeight: true
-		}
-	}
-]
-});
+// 		}
+// 	},
+// 	{
+// 		breakpoint: 640,
+// 		settings: {
+// 			slidesToShow: 1,
+// 			slidesToScroll: 1,
+// 			arrows: true,
+// 			adaptiveHeight: true
+// 		}
+// 	}
+// ]
+// });
+
+
+
+
+
+
+
+
+
 
 var element = document.querySelector('.ordeh');
-var element2 = document.querySelector('.ordeh2');
 var Visible = function (target) {
   var targetPosition = {
       top: window.pageYOffset + target.getBoundingClientRect().top,
