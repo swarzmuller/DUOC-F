f5d28df849a34aaee94f3a2229bac5053face10a
Vitalii <vitalii.ilchenko@gmail.com>
2019-08-13 16:09:15 +0300

    slider imgs

--- app/js/main.js
+++ app/js/main.js
@@ -4,44 +4,6 @@
 //=include ../components/footer/footer.js
 
 
-
-
-// $('.increase-bottom').slick({
-// 	slidesToShow: 3,
-// 	slidesToScroll: 3,
-// 	arrows: false,
-// 	responsive: [
-// 	{
-// 		breakpoint: 1024,
-// 		settings: {
-// 			slidesToShow: 2,
-// 			slidesToScroll: 2,
-// 			arrows: true
-// 		}
-// 	},
-// 	{
-// 		breakpoint: 767,
-// 		settings: {
-// 			slidesToShow: 2,
-// 			slidesToScroll: 2,
-// 			arrows: true
-
-// 		}
-// 	},
-// 	{
-// 		breakpoint: 640,
-// 		settings: {
-// 			slidesToShow: 1,
-// 			slidesToScroll: 1,
-// 			arrows: true,
-// 			adaptiveHeight: true
-// 		}
-// 	}
-// ]
-// });
-
-
-
 const haburg = document.querySelector('.hamburger');
 const menu = document.querySelector('.header__menu');
 const close = document.querySelector('.close');
